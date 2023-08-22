5eb0985cfa1703ecc06c94bf4653525bce312ea0
YaRkyungmin <kkgm94@naver.com>
2023-08-23 03:28:11 +0900

    ♻️ Refactor: GameManager 수정

--- ios-refactoring-rock-paper-scissors/GameManager.swift
+++ ios-refactoring-rock-paper-scissors/GameManager.swift
@@ -6,7 +6,43 @@
 //
 
 typealias GameInformation = (gameTurn: GameTurn, gameStatus: GameStatus)
+typealias HandShapeInformation = (handShape: HandShape, gameStatus: GameStatus)
 
 struct GameManager {
-    private var gameTurn: GameTurn
+    func playGame() {
+        var gameInformation: GameInformation = (.unknown, .execute)
+        
+        gameInformation = RockScissorsPaper(gameInformation)
+        gameInformation = MuckJjiBbaManager(gameInformation)
+        
+        show(gameInformation)
+    }
+    
+    private func RockScissorsPaper(_ gameInformation: GameInformation) -> GameInformation {
+        let rockScissorsPaperManager = RockScissorsPaperManager()
+        
+        return rockScissorsPaperManager.playRockScissorsPaper(gameInformation)
+    }
+    
+    private func MuckJjiBbaManager(_ gameInformation: GameInformation) -> GameInformation {
+        let mukJjiBbaManager = MukJjiBbaManager()
+        
+        return mukJjiBbaManager.playMukJjiBba(gameInformation)
+    }
+    
+    private func show(_ gameInformation: GameInformation) {
+        switch gameInformation.gameTurn {
+        case .unknown:
+            print(NameSpace.endMessage)
+        default:
+            print(String(format: NameSpace.resultMessage, gameInformation.gameTurn.description))
+        }
+    }
+}
+
+extension GameManager {
+    private enum NameSpace {
+        static let endMessage = "게임 종료"
+        static let resultMessage = "%@의 승리!"
+    }
 }
