c0569d845ea8a3863ac21765fd34dcb8618b257d
YaRkyungmin <kkgm94@naver.com>
2023-08-23 03:28:45 +0900

    ✨ Feat: GameTurn타입내 description 프로퍼티 추가

--- ios-refactoring-rock-paper-scissors/GameTurn.swift
+++ ios-refactoring-rock-paper-scissors/GameTurn.swift
@@ -9,4 +9,15 @@ enum GameTurn {
     case user
     case computer
     case unknown
+    
+    var description: String {
+        switch self {
+        case .user:
+            return "사용자"
+        case .computer:
+            return "컴퓨터"
+        case .unknown:
+            return "알 수 없음"
+        }
+    }
 }
